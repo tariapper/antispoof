e61504646a873991fcdee42c50016771f2850028
tariapper <tariapper@users.noreply.github.com>
2020-02-27 22:19:21 -0500

    initial commit

---
+++ app/src/main/java/com/example/antispoof/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.antispoof;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    final Handler handler_interact = new Handler();
+    View bkg;// = findViewById(R.id.screen);
+    View half;// = findViewById(R.id.half);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setBrightness(bkg, 255);
+
+        bkg = findViewById(R.id.screen);
+        half = findViewById(R.id.half);
+
+        final Button button = findViewById(R.id.start);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+            }
+        });
+
+        // The request code used in ActivityCompat.requestPermissions()
+        // and returned in the Activity's onRequestPermissionsResult()
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_SETTINGS
+        };
+
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        //creating timer
+        Timer timer_interact = new Timer();
+        timer_interact.schedule(new TimerTask() {
+            @Override
+            public void run() {UpdateGUI();}
+        }, 3000);
+    }
+
+    private void UpdateGUI() {
+        handler_interact.post(runnable_interact);
+    }
+
+    //creating runnable
+    final Runnable runnable_interact = new Runnable() {
+        public void run() {
+            bkg.setBackgroundColor(Color.GREEN);
+            half.setBackgroundColor(Color.RED);
+        }
+    };
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void setBrightness(View view,int brightness) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.System.canWrite(getApplicationContext())) {
+                brightness = 255;
+
+                ContentResolver cResolver = this.getApplicationContext().getContentResolver();
+                Settings.System.putInt(cResolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+
+            } else {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        }
+
+    }
+
+
+
+}
